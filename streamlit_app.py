a2a5e1cdcf58052215e14ab18d9aeee20e2d0aae
Hengyuan Liu <108158742+DavidLiu0619@users.noreply.github.com>
2024-05-04 10:21:43 -0700

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,207 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+from shapely.geometry import LineString, Polygon
+import copy
+from shapely.geometry import Point, Polygon
+from shapely.geometry.polygon import LinearRing, LineString
+from io import BytesIO
+
+
+# Function to plot the coordinates
+def plot_coords(ax, ob):
+    x, y = ob.xy
+    ax.plot(x, y, '.', color='#999999', zorder=1)
+
+# Function to plot lines
+def plot_line(ax, ob):
+    x, y = ob.xy
+    ax.plot(x, y, color='cyan', alpha=0.7, linewidth=3, solid_capstyle='round', zorder=2)
+
+# Function to print the border and race line
+def print_border(ax, waypoints, inner_border_waypoints, outer_border_waypoints):
+    line = LineString(waypoints)
+    plot_coords(ax, line)
+    plot_line(ax, line)
+
+    line = LineString(inner_border_waypoints)
+    plot_coords(ax, line)
+    plot_line(ax, line)
+
+    line = LineString(outer_border_waypoints)
+    plot_coords(ax, line)
+    plot_line(ax, line)
+
+def menger_curvature(pt1, pt2, pt3, atol=1e-3):
+    vec21 = np.array(pt1) - np.array(pt2)
+    vec23 = np.array(pt3) - np.array(pt2)
+    norm21 = np.linalg.norm(vec21)
+    norm23 = np.linalg.norm(vec23)
+    theta = np.arccos(np.dot(vec21, vec23) / (norm21 * norm23))
+    if np.isclose(theta - np.pi, 0.0, atol=atol):
+        theta = 0.0
+    dist13 = np.linalg.norm(np.array(pt1) - np.array(pt3))
+    return 2 * np.sin(theta) / dist13 if dist13 != 0 else 0
+
+
+def improve_race_line(old_line, inner_border, outer_border):
+    '''Use gradient descent, inspired by K1999, to find the racing line'''
+    # start with the center line
+    new_line = copy.deepcopy(old_line)
+    ls_inner_border = Polygon(inner_border)
+    ls_outer_border = Polygon(outer_border)
+    for i in range(0,len(new_line)):
+        xi = new_line[i]
+        npoints = len(new_line)
+        prevprev = (i - 2 + npoints) % npoints
+        prev = (i - 1 + npoints) % npoints
+        nexxt = (i + 1 + npoints) % npoints
+        nexxtnexxt = (i + 2 + npoints) % npoints
+        #print("%d: %d %d %d %d %d" % (npoints, prevprev, prev, i, nexxt, nexxtnexxt))
+        ci = menger_curvature(new_line[prev], xi, new_line[nexxt])
+        c1 = menger_curvature(new_line[prevprev], new_line[prev], xi)
+        c2 = menger_curvature(xi, new_line[nexxt], new_line[nexxtnexxt])
+        target_ci = (c1 + c2) / 2
+        #print("i %d ci %f target_ci %f c1 %f c2 %f" % (i, ci, target_ci, c1, c2))
+
+        # Calculate prospective new track position, start at half-way (curvature zero)
+        xi_bound1 = copy.deepcopy(xi)
+        xi_bound2 = ((new_line[nexxt][0] + new_line[prev][0]) / 2.0, (new_line[nexxt][1] + new_line[prev][1]) / 2.0)
+        p_xi = copy.deepcopy(xi)
+        for j in range(0,XI_ITERATIONS):
+            p_ci = menger_curvature(new_line[prev], p_xi, new_line[nexxt])
+            #print("i: {} iter {} p_ci {} p_xi {} b1 {} b2 {}".format(i,j,p_ci,p_xi,xi_bound1, xi_bound2))
+            if np.isclose(p_ci, target_ci):
+                break
+            if p_ci < target_ci:
+                # too flat, shrinking track too much
+                xi_bound2 = copy.deepcopy(p_xi)
+                new_p_xi = ((xi_bound1[0] + p_xi[0]) / 2.0, (xi_bound1[1] + p_xi[1]) / 2.0)
+                if Point(new_p_xi).within(ls_inner_border) or not Point(new_p_xi).within(ls_outer_border):
+                    xi_bound1 = copy.deepcopy(new_p_xi)
+                else:
+                    p_xi = new_p_xi
+            else:
+                # too curved, flatten it out
+                xi_bound1 = copy.deepcopy(p_xi)
+                new_p_xi = ((xi_bound2[0] + p_xi[0]) / 2.0, (xi_bound2[1] + p_xi[1]) / 2.0)
+
+                # If iteration pushes the point beyond the border of the track,
+                # just abandon the refinement at this point.  As adjacent
+                # points are adjusted within the track the point should gradually
+                # make its way to a new position.  A better way would be to use
+                # a projection of the point on the border as the new bound.  Later.
+                if Point(new_p_xi).within(ls_inner_border) or not Point(new_p_xi).within(ls_outer_border):
+                    xi_bound2 = copy.deepcopy(new_p_xi)
+                else:
+                    p_xi = new_p_xi
+        new_xi = p_xi
+        # New point which has mid-curvature of prev and next points but may be outside of track
+        #print((new_line[i], new_xi))
+        new_line[i] = new_xi
+    return new_line
+
+def create_download_link(loop_race_line):
+    """Create a download link for the numpy array."""
+    buffer = BytesIO()
+    np.save(buffer, loop_race_line)
+    buffer.seek(0)
+    return buffer
+
+
+
+########################################################################################################
+# Streamlit app layout
+st.title('Race Line Visualization')
+
+uploaded_file = st.file_uploader("Upload your track file (.npy)", type="npy")
+if uploaded_file is not None:
+    waypoints = np.load(uploaded_file, allow_pickle=True)
+    # Convert to Shapely objects
+    center_line = waypoints[:,0:2]
+    inner_border = waypoints[:,2:4]
+    outer_border = waypoints[:,4:6]
+
+    # Plotting
+    fig, ax = plt.subplots(figsize=(16, 10), facecolor='black')
+    ax.set_aspect('equal')
+    ax.set_facecolor('black')  # Set the axes background color
+    fig.patch.set_facecolor('black')  # Set the figure background color
+    # Remove axis ticks
+    ax.tick_params(axis='both', colors='white')  # Make ticks white
+    # Set grid and labels with appropriate colors if necessary
+    ax.xaxis.label.set_color('white')
+    ax.yaxis.label.set_color('white')
+    ax.grid(True, which='both', color='gray', linestyle='--', linewidth=0.5)  # Optional grid
+    print_border(ax, center_line, inner_border, outer_border)
+    # Use Streamlit's function to display the plot
+    st.pyplot(fig)
+
+    
+    # Set default iteration values
+    #LINE_ITERATIONS = 1000
+    #XI_ITERATIONS = 3
+    # Set default values and create sliders for dynamic adjustments
+    st.write("## Choose your Hyperparameters:")
+    st.markdown("- Number of Line Iterations: Number of times to scan the entire race track to iterate")
+    st.markdown("- Xi Iterations: Number of times to iterate each new race line point")
+
+    LINE_ITERATIONS = st.slider('Number of Line Iterations', min_value=100, max_value=2000, value=500, step=100)
+    XI_ITERATIONS = st.slider('Xi Iterations', min_value=3, max_value=10, value=5)
+
+    if st.button('Calculate Optimal Race Line'):
+        race_line = copy.deepcopy(center_line[:-1])  # Start with a deep copy of the centerline
+
+        # Initialize a progress bar
+        progress_bar = st.progress(0)
+        status_text = st.empty()
+
+        for i in range(LINE_ITERATIONS):
+            race_line = improve_race_line(race_line, inner_border, outer_border)
+            
+            # Update progress bar and status text every 20 iterations
+            if i % 20 == 0:
+                progress_percentage = int(100 * (i / LINE_ITERATIONS))
+                progress_bar.progress(progress_percentage)
+                status_text.text(f"Computing... Iteration {i} of {LINE_ITERATIONS}")
+
+        # Complete the progress
+        progress_bar.progress(100)
+        status_text.text("Calculation completed!")
+
+        # Closing the loop to make the race line continuous
+        loop_race_line = np.append(race_line, [race_line[0]], axis=0)
+
+        # Display shapes and lengths
+        original_length = LineString(center_line).length
+        new_length = LineString(loop_race_line).length
+        
+        st.write(f"Original centerline length: {original_length:.2f}")
+        st.write(f"New race line length: {new_length:.2f}")
+        st.write("## This is your Optimal Race Line")
+
+        # Plotting the track
+        fig, ax = plt.subplots(figsize=(16, 10), facecolor='black')
+        ax.set_aspect('equal')
+        ax.set_facecolor('black')  # Set the axes background color
+        fig.patch.set_facecolor('black')  # Set the figure background color
+        # Remove axis ticks
+        ax.tick_params(axis='both', colors='white')  # Make ticks white
+        # Set grid and labels with appropriate colors if necessary
+        ax.xaxis.label.set_color('white')
+        ax.yaxis.label.set_color('white')
+        ax.grid(True, which='both', color='gray', linestyle='--', linewidth=0.5)  # Optional grid
+        # Printing border and race line on the plot
+        print_border(ax, loop_race_line, inner_border, outer_border)
+        st.pyplot(fig)
+
+   
+
+        # Provide download button
+        buffer = create_download_link(loop_race_line)
+        st.download_button(
+            label="Download Optimal Race Line as .npy",
+            data=buffer,
+            file_name="optimal_track.npy",
+            mime="application/octet-stream"
+        )
