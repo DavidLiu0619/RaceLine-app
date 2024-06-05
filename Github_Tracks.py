cdde79c68b20c485462bdc78a5d3be75cc2fd706
Hengyuan Liu <108158742+DavidLiu0619@users.noreply.github.com>
2024-06-05 10:51:48 -0700

    Update Github_Tracks.py

--- Github_Tracks.py
+++ Github_Tracks.py
@@ -163,6 +163,52 @@ def create_download_link(loop_race_line):
     np.save(buffer, loop_race_line)
     buffer.seek(0)
     return buffer
+
+def circle_radius(coords):
+    x1, y1, x2, y2, x3, y3 = [i for sub in coords for i in sub]
+    a = x1*(y2-y3) - y1*(x2-x3) + x2*y3 - x3*y2
+    b = (x1**2+y1**2)*(y3-y2) + (x2**2+y2**2)*(y1-y3) + (x3**2+y3**2)*(y2-y1)
+    c = (x1**2+y1**2)*(x2-x3) + (x2**2+y2**2)*(x3-x1) + (x3**2+y3**2)*(x1-x2)
+    d = (x1**2+y1**2)*(x3*y2-x2*y3) + (x2**2+y2**2) * (x1*y3-x3*y1) + (x3**2+y3**2)*(x2*y1-x1*y2)
+    try:
+        r = abs((b**2+c**2-4*a*d) / abs(4*a**2)) ** 0.5
+    except:
+        r = 999
+    return r
+
+def circle_indexes(mylist, index_car, add_index_1=0, add_index_2=0):
+    list_len = len(mylist)
+    index_1 = (index_car + add_index_1) % list_len
+    index_2 = (index_car + add_index_2) % list_len
+    return [index_car, index_1, index_2]
+
+def optimal_velocity(track, min_speed, max_speed, look_ahead_points):
+    radius = []
+    for i in range(len(track)):
+        indexes = circle_indexes(track, i, add_index_1=-1, add_index_2=1)
+        coords = [track[indexes[0]], track[indexes[1]], track[indexes[2]]]
+        radius.append(circle_radius(coords))
+    v_min_r = min(radius)**0.5
+    constant_multiple = min_speed / v_min_r
+    if look_ahead_points == 0:
+        max_velocity = [(constant_multiple * i**0.5) for i in radius]
+        velocity = [min(v, max_speed) for v in max_velocity]
+        return velocity
+    else:
+        LOOK_AHEAD_POINTS = look_ahead_points
+        radius_lookahead = []
+        for i in range(len(radius)):
+            next_n_radius = []
+            for j in range(LOOK_AHEAD_POINTS+1):
+                index = circle_indexes(mylist=radius, index_car=i, add_index_1=j)[1]
+                next_n_radius.append(radius[index])
+            radius_lookahead.append(min(next_n_radius))
+        max_velocity_lookahead = [(constant_multiple * i**0.5) for i in radius_lookahead]
+        velocity_lookahead = [min(v, max_speed) for v in max_velocity_lookahead]
+        return velocity_lookahead
+    
+def dist_2_points(x1, x2, y1, y2):
+    return abs(abs(x1-x2)**2 + abs(y1-y2)**2)**0.5
 #####################################################################
 st.sidebar.title("Navigation")
 page = st.sidebar.radio("Go to", ["Origanl & Optimal Race Line Visualization", "Optimal Speed Calculation"])
